a098988f4bc3f36ac097212cb579b0b2d4c292a6
shinpekong <53926986+shinpekong@users.noreply.github.com>
2021-07-30 09:45:16 +0800

    Update kms.sh

--- kms.sh
+++ kms.sh
@@ -137,14 +137,14 @@ install_main() {
 
     if [[ x"${release}" == x"centos" ]]; then
         yum -y install gcc git make nss curl libcurl
-	if ! wget --no-check-certificate -O /etc/init.d/kms https://github.com/shinpekong/autokms/blob/main/kms-centos; then
+	if ! wget --no-check-certificate -O /etc/init.d/kms https://raw.githubusercontent.com/shinpekong/autokms/main/kms-centos; then
             echo -e "[${red}Error:${plain}] Failed to download KMS Server script."
             exit 1
         fi
     elif [[ x"${release}" == x"debian" || x"${release}" == x"ubuntu" ]]; then
         apt-get -y update
         apt-get install -y gcc git make libnss3 curl libcurl3-nss
-	if ! wget --no-check-certificate -O /etc/init.d/kms https://github.com/shinpekong/autokms/blob/main/kms-debian; then
+	if ! wget --no-check-certificate -O /etc/init.d/kms https://raw.githubusercontent.com/shinpekong/autokms/main/kms-debian; then
             echo -e "[${red}Error:${plain}] Failed to download KMS Server script."
             exit 1
         fi
